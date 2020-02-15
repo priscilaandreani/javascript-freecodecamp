2e12fcbafb9561f9baf7375c7f1d7991d01886d9
Priscila Andreani <priscilaandreani@gmail.com>
2020-02-15 14:08:34 -0300

    switch and objects
    basics: switch and objects

---
+++ accessing-nested-objects.js
@@ -0,0 +1,16 @@
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+
+// deve-se usar [] porque a propriedade possui espaço
---
+++ manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+];
+
+myMusic.push({
+  "artist": "Me",
+  "title": "My best album",
+  "release_year": 2020,
+  "formats": [
+    "spotify",
+    "deezer",
+    "itunes",
+  ]}
+)
